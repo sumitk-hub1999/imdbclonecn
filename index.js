6a97d76e132c7dd560d717f62b9fffc54a1aad5e
sumitk-hub1999 <skatyal1999@gmail.com>
2023-01-20 15:46:31 +0530

    basic layout

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const searchBox = document.getElementById("search");
 const searchButton = document.getElementById("search-button");
 const searchList = document.getElementById("search-list");
 const resultGrid = document.getElementById("result-grid");
-searchBox.addEventListener("click", fi);
-searchButton.addEventListener("click", find);
+// searchBox.addEventListener("click", fi);
+// searchButton.addEventListener("click", find);
 async function loadMovie(searchterm) {
-  const url = `https://www.omdbapi.com/?s=${searchterm}&page=1&apikey=8be51894`;
-  const res = await fetch(`${url}`);
+  const link = `https://www.omdbapi.com/?s=${searchterm}&page=1&apikey=8be51894`;
+  const res = await fetch(`${link}`);
   const data = await res.json();
-  if (data.Response == "True") displayMovieList(data.Search);
+  console.log(data);
+  if (data.Response == "True") {
+    console.log(data);
+    displaySearchList(data.Search);
+  }
 }
+console.log(loadMovie("seabiscuit"));
+// const url = `https://www.omdbapi.com/?s=${searchterm}&page=1&apikey=8be51894`;
+// let movies = [];
+// fetch(url)
+// .then((end)=>end.json())
+// .then((data)=>)
